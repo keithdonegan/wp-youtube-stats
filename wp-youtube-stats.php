4974c45b036517e9e76200450298aa6daa158e5b
Keith <keith.donegan@gmail.com>
2025-04-18 23:27:54 +0100

    Update wp-youtube-stats.php

--- wp-youtube-stats.php
+++ wp-youtube-stats.php
@@ -1,1 +1,7 @@
-//coming soon
+<?php
+// Add this to your theme's functions.php file or create a custom plugin
+
+// Function to fetch YouTube channel statistics
+function get_youtube_channel_stats($channel_id = null) {
+    return $stats;
+}
