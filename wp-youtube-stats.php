8e4ef7a5aa003edeb5c7bc9f78ff5fa86dcc9435
Keith Donegan <keith.donegan@gmail.com>
2025-07-25 01:32:14 +0100

    working

--- wp-youtube-stats.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Add this to your theme's functions.php file or create a custom plugin
-
-// Function to fetch YouTube channel statistics
-function get_youtube_channel_stats($channel_id = null) {
-    return $stats;
-}
